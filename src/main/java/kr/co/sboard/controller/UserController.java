2dba54440d2a67cb7ee03e568c7ae6b9e464ca22
Han Seong Ju <gkstjdwntl@gmail.com>
2026-06-29 17:10:28 +0900

    commit 0.0.1

--- src/main/java/kr/co/sboard/controller/UserController.java
+++ src/main/java/kr/co/sboard/controller/UserController.java
@@ -1,14 +1,18 @@
 package kr.co.sboard.controller;
 
+import kr.co.sboard.dto.AppInfoDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Log4j2
 @RequiredArgsConstructor
 @Controller
 public class UserController {
+
+    private final AppInfoDTO appInfo;
 
     @GetMapping("/user/info")
     public String info(){
