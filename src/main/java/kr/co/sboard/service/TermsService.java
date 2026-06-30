057b56c880eb960b19fadca840a1cb4472f6913e
Han Seong Ju <gkstjdwntl@gmail.com>
2026-06-30 11:40:35 +0900

    0.0.2

--- src/main/java/kr/co/sboard/service/TermsService.java
+++ src/main/java/kr/co/sboard/service/TermsService.java
@@ -4,14 +4,15 @@ import kr.co.sboard.dao.ArticleDAO;
 import kr.co.sboard.dao.TermsDAO;
 import kr.co.sboard.dto.ArticleDTO;
 import kr.co.sboard.dto.TermsDTO;
+import kr.co.sboard.entity.Terms;
 import kr.co.sboard.repository.ArticleRepository;
 import kr.co.sboard.repository.TermsRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -21,30 +22,31 @@ public class TermsService {
     private final TermsDAO dao;
     private final TermsRepository repository;
 
-    @GetMapping("")
     public TermsDTO get(int no){
-        return null;
-    }
+        // Mybatis
+        TermsDTO termsDTO = dao.select(1);
 
-    @GetMapping("")
+        // JPA
+        /*
+        Optional<Terms> optTerms = repository.findById(1);
+
+        if(optTerms.isPresent()){
+            Terms entity = optTerms.get();
+            return entity.toDTO();
+        }
+        */
+        return termsDTO;
+    }
     public List<TermsDTO> getAll(){
         return null;
     }
-
-    @GetMapping("")
     public void register(TermsDTO dto){
 
     }
-
-    @GetMapping("")
     public void modify(TermsDTO dto){
 
     }
-
-    @GetMapping("")
     public void remove(int no){
 
     }
-
-
 }
